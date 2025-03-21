6027ffb35c4d6d36451410da4d0305e14a0c58b7
Najshree <81029735+NeplayGames@users.noreply.github.com>
2025-03-21 10:53:02 -0500

    Added new system and worked on lighting

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using HorrorGame.Weapon;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,36 +11,58 @@ namespace HorrorGame.Player
     [SerializeField] private CharacterController characterController;
     [SerializeField] private float speed = 3f;
     [SerializeField] private float mouseRotationSpeed = 5f;
+    [SerializeField, Range(1,5f)] private float jumpValue = 2f;
     [SerializeField] private Gun gun;
     private Vector3 movementInputValue = Vector2.zero;
     private Vector2 mouseDelta = Vector2.zero;
     private Transform cameraTransform => Camera.main.transform;
     private float mouseDeltaY = 0f;
     private WeaponController weaponController;
-    void Start(){
+
+    private bool isSprinting = false;
+    private bool isJumping = false;
+    void Start()
+    {
       Cursor.lockState = CursorLockMode.Locked;
       weaponController = new WeaponController();
       weaponController.SetCurrentHoldingWeapon(gun);
     }
     void Update()
     {
+      CheckForGroundAndJump();
       Move();
       Rotate();
     }
 
-    private void Move(){
+    private void CheckForGroundAndJump()
+    {
+      if (characterController.isGrounded)
+      {
+        if(isJumping){
+          movementInputValue.y = jumpValue;
+          isJumping = false;
+        }
+        return;
+      }
+      movementInputValue.y += Physics.gravity.y * Time.deltaTime;
+    }
+
+    private void Move()
+    {
       Vector3 moveVal = transform.TransformDirection(movementInputValue);
-      characterController.Move(moveVal * Time.deltaTime * speed);
+      characterController.Move(moveVal * Time.deltaTime * speed * (isSprinting ? 1.5f : 1));
     }
 
-    private void Rotate(){
-        transform.Rotate(0, mouseDelta.x, 0);
-        mouseDeltaY -= mouseDelta.y;
-        mouseDeltaY = Mathf.Clamp(mouseDeltaY, -60, 60);
-        cameraTransform.localRotation = Quaternion.Euler(mouseDeltaY, 0 ,0);
+    private void Rotate()
+    {
+      transform.Rotate(0, mouseDelta.x, 0);
+      mouseDeltaY -= mouseDelta.y;
+      mouseDeltaY = Mathf.Clamp(mouseDeltaY, -60, 60);
+      cameraTransform.localRotation = Quaternion.Euler(mouseDeltaY, 0, 0);
     }
 
-    private void UseWeapon(){
+    private void UseWeapon()
+    {
       weaponController.Shoot();
     }
     private void OnMovement(InputValue inputValue)
@@ -48,14 +71,19 @@ namespace HorrorGame.Player
       movementInputValue = new Vector3(value.x, 0, value.y);
     }
 
-    private void OnMouseDelta(InputValue inputValue){
+    private void OnMouseDelta(InputValue inputValue)
+    {
       mouseDelta = inputValue.Get<Vector2>();
       mouseDelta = mouseDelta * Time.deltaTime * mouseRotationSpeed;
     }
 
-    private void OnShoot(){
+    private void OnShoot()
+    {
       UseWeapon();
     }
+
+    private void OnSprint() => isSprinting = !isSprinting;
+    private void OnJump() => isJumping = true;
   }
 
 }
